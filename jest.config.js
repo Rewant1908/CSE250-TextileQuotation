6f03494c5a86d42dd09df87fe33386ea57825635
Rewant Agrawal <rewant.a@ahduni.edu.in>
2026-04-10 15:25:16 +0530

    remove: delete jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('jest').Config} */
-const config = {
-    testEnvironment: 'node',
-    transform: {},
-    testMatch: ['**/tests/**/*.test.js'],
-    forceExit: true
-};
-
-export default config;
